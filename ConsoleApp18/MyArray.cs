fab88a354cdff6710726c71afccc406888f8dd80
Віталій Чуй <vitalii.chui@gmail.com>
2022-12-24 20:38:14 +0200

    Add project files.

---
+++ ConsoleApp18/MyArray.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class MyArray<T>:IEnumerable
+    {
+        private T[] array = null;
+        public MyArray() => array = new T[0];
+        public MyArray(T[] array) => this.array = array;
+
+        public void Add(T val)
+        {
+            T[] temp = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                array[i] = temp[i];
+            }
+            array[array.Length - 1] = val;
+        }
+
+        public void Append(T[] arr)
+        {
+            T[] temp = array;
+            array= new T[array.Length + arr.Length];
+            int index = 0;
+            foreach(T element in temp)
+            {
+                array[index] = element;
+                index++;
+            }
+            foreach (T element in arr)
+            {
+                array[index] = element;
+                index++;
+            }
+        }
+        public void Remove(T element)
+        {
+            T[] temp = array;
+            int count = 0;
+            foreach (T item in temp)
+            {
+                if (element.Equals(item))
+                    count++;
+            }
+            if (count > 0)
+            {
+                array = new T[array.Length - count];
+                count = 0;
+                foreach (T val in temp)
+                {
+                    if (!element.Equals(val))
+                    {
+                        array[count] = val;
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No such value found");
+            }
+        }
+
+        public void Clear() => array = new T[0];
+        public int FindElementIndex(T element)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (element.Equals(array[i]))
+                    return i;
+            }
+            return -1;
+        }
+        
+        public int[] FindElementIndexes(T element)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (element.Equals(array[i]))
+                {
+                    count++;
+                }
+            }
+            int[] arr = new int[array.Length-count];
+            count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (element.Equals(array[i]))
+                {
+                    arr[count] = i;
+                    count++;
+                }
+            }
+            return arr;
+        }
+        public void Sort() => Array.Sort(array);
+        public IEnumerator GetEnumerator() { 
+            return array.GetEnumerator(); 
+        }
+        public new void ToString()
+        {
+            Console.Write("[");
+            foreach(T element in array)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine("]");
+        }
+
+        public T this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray<int> arr = new MyArray<int>(new int[] { 3, 5, 7, 3, 4, 8 });
+            arr.ToString();
+            arr.Add(1);
+            arr.ToString();
+            arr.Append(new int[] { 4, 5, 6, 10, 11, 15 });
+            arr.ToString();
+            arr.Sort();
+            arr.ToString();
+            arr.Remove(4);
+            arr.ToString();
+        }
+    }
+}
